72425ce760da97e8a12a3c5f76c0728e55a43b21
Lakshya Shrivastava <69989952+LakshyaShrivastava@users.noreply.github.com>
2025-01-24 01:15:07 -0800

    brute force pwd cracker

---
+++ bruteForce.py
@@ -0,0 +1,20 @@
+import hashlib
+import itertools
+import string
+
+DEBUG_PRINT = True
+
+def brute_force(hash_to_crack, max_length):
+    # characters to include in brute force
+    characters = string.ascii_lowercase + string.digits
+
+    for length in range(1, max_length + 1):
+        # generate all possible combinations
+        for guess in itertools.product(characters, repeat=length):
+            guess_word = ''.join(guess)
+            if DEBUG_PRINT:
+                print("Guessing " + guess_word)
+            hashed_guess = hashlib.sha256(guess_word.encode()).hexdigest()
+            if hashed_guess == hash_to_crack:
+                return guess_word
+    return None
---
+++ main.py
@@ -0,0 +1,14 @@
+import hashlib
+from bruteForce import brute_force
+
+# brute_force_test
+def brute_force_test():
+    target_hash = hashlib.sha256(b"abc123").hexdigest
+    cracked_password = brute_force(target_hash, max_length=6)
+    if cracked_password:
+        print(f"Password cracked: {cracked_password}")
+    else:
+        print("Password not found.")
+
+if __name__ == '__main__':
+    brute_force_test()
